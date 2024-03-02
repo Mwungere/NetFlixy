ca86886c9e48d694e772059c3edc5d939f1f8e58
Mwungere <mwungereelite56@gmail.com>
2024-03-02 12:46:34 +0200

    signupo

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import MovieItem from './MovieItem';
 
 const MovieRow = ({ title, url }) => {
   const [movies, setMovies] = useState([]);
@@ -12,6 +13,13 @@ const MovieRow = ({ title, url }) => {
   return (
     <>
       <h2 className=' font-nsans-bold md:text-xl p-4 capitalize'>{title}</h2>
+      <div className=' relative flex items-center'>
+        <div id={'slider'} className=' w-full h-full overflow-x-scroll whitespace-nowrap  scroll-smooth scrollbar-hide'>
+          {movies.map((movie) =>(
+            <MovieItem movie={movie} key={movie.id}/>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className=' w-full h-screen'>
+      <img
+       src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/4979868a-17d6-44d7-b573-a8293d3f3b29/RW-en-20240226-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+        alt="///"
+        className=' hidden sm:block absolute w-full h-full object-cover'
+         />
+         <div className=' bg-black/70 fixed top-0 left-0 w-full h-screen' />
+    </div>
   )
 }
 
